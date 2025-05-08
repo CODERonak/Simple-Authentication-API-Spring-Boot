cd535ec210ee1815c09798d1ded78b1efbf2bc9b
r9988.2588 <r9988.2588@gmail.com>
2025-05-08 08:15:00 +0000

    first commit

---
+++ src/main/java/com/code/SimpleAuthenticationAPI/SimpleAuthenticationApiApplication.java
@@ -0,0 +1,12 @@
+package com.code.SimpleAuthenticationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthenticationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAuthenticationApiApplication.class, args);
+	}
+}
